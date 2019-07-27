6751da34be7bdec22846a30e6e16cfb302780a52
atabana <ahmedtabana10@gmail.com>
2019-07-26 22:37:14 -0400

    add book-list, book-detail and book-form components.

---
+++ src/app/book-form/book-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bs-book-form',
+  templateUrl: './book-form.component.html',
+  styles: []
+})
+export class BookFormComponent implements OnInit {
+
+  book = {
+    title: "dummy title",
+    description: "dummy description",
+    unitCost: "123",
+    isbn: "123-3456-567",
+    nbOfPages: "456",
+    language: "English",
+    imageURL: "http://ecx.images-amazon.com/images/I/51amFVZbyKL._SL160_.jpg"
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
